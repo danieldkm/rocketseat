19cdc2d6663811a8a41ab0d03a54459e61cfa855
Daniel <daniel.morita@kroton.com.br>
2020-01-20 08:47:13 -0300

    .

--- omnistack10/web/src/App.js
+++ omnistack10/web/src/App.js
@@ -1,24 +1,41 @@
 import React, { useState } from 'react';
-
-// import Header from './Header';
-
-// Componente: bloco isolado de HTML, CSS e JS, o qual não interfere no restante da aplicação.
-// Propriedade: informações que um componente PAI passa para o componente FILHO.
-// Estado: informações mantidas pelo componente (Lembrar: imutabilidade).
+import './global.css';
+import './App.css';
+import './Sidebar.css';
 
 function App() {
-
-  const [counter, setCounter] = useState(0);
-
-  function incrementCounter() {
-    setCounter(counter + 1);
-  }
-
   return (
-    <>
-      <h1>Contador: {counter}</h1>
-      <button onClick={incrementCounter}>Incrementar</button>
-    </>
+    <div id="app">
+      <aside>
+        <strong>Cadastro</strong>
+        <form>
+          <div className="input-block">
+            <label htmlFor="github_username">Usuário do Github</label>
+            <input name="github_username" id="github_username" required/>
+          </div>
+
+          <div className="input-block">
+            <label htmlFor="techs">Tecnologias</label>
+            <input name="techs" id="techs" required/>
+          </div>
+
+          <div className="input-group">
+            <div className="input-block">
+              <label htmlFor="latitude">Latidude</label>
+              <input name="latitude" id="latitude" required/>
+            </div>
+            <div className="input-block">
+              <label htmlFor="longitude">Longitude</label>
+              <input name="longitude" id="longitude" required/>
+            </div>
+          </div>
+          <button type="submit">Salvar</button>
+        </form>
+      </aside>
+      <main>
+
+      </main>
+    </div>
   );
 }
 
